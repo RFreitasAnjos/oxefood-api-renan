9408a828780a5947dd7ae28c554b2c54d14bf821
aluno <aluno@ifpe>
2025-04-03 20:50:39 -0300

    Enviando arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRenanApplication.java
@@ -0,0 +1,12 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiRenanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiRenanApplication.class, args);
+	}
+}
