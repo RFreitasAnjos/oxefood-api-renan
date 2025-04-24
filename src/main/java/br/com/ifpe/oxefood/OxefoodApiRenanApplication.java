8c636ba40b6eaaa46d4da7034494d09b583b3434
aluno <aluno@ifpe>
2025-04-24 20:20:13 -0300

    Create endpoints and comunications

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiRenanApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiRenanApplication.java
@@ -2,8 +2,10 @@ package br.com.ifpe.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //Preencher os dados de auditoria
 public class OxefoodApiRenanApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -2,6 +2,11 @@ package br.com.ifpe.oxefood.util.entity;
 
 import java.io.Serializable;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +14,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = {"id"})
+@MappedSuperclass
 public abstract class EntidadeNegocio implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private Boolean habilitado;
 }
