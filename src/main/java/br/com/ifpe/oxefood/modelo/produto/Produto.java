9bc6b24ba3aa1ed042e4b7766973e8a930e57a05
aluno <aluno@ifpe>
2025-05-08 19:15:11 -0300

    Add entity produto and entregador

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/Produto.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/Produto.java
@@ -1,5 +1,23 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
-public class Produto {
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name= "Produto")
+//@SQLRestriction("habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Produto extends EntidadeAuditavel{
+    private String nomeProduto;
 
 }
