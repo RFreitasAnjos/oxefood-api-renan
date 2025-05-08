1ef97351c12e033d8f407242eee8dd25b42a650c
aluno <aluno@ifpe>
2025-05-08 19:02:05 -0300

    Add methods listarTodos findByID clients

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.oxefood.modelo.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +17,13 @@ public class ClienteService {
     public Cliente save(Cliente cliente){
         cliente.setHabilitado(Boolean.TRUE);
         return repository.save(cliente);
+    }
+    
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Cliente obterPorID(Long id) {
+        return repository.findById(id).get();
     }
 }
