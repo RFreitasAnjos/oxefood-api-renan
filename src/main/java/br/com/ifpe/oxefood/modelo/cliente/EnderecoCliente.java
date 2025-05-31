a0dd13c7489dd18bbf490a8c5f74e88300bd1a69
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-05-30 21:37:06 -0300

    Fix: Removed EnderecoCliente duplicated and fix EnderecoCliente

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/EnderecoCliente.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/EnderecoCliente.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Cliente")
+@Table(name = "EnderecoCliente")
 @SQLRestriction("habilitado = true")
 @Builder
 @Getter
