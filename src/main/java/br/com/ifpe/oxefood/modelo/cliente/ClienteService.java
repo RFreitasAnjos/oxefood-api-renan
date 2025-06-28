f1cd47467ea202a79b8dfc301c1f1ccf71dff938
Renan Freitas <renan.freitas.anjos@gmail.com>
2025-06-27 21:41:40 -0300

    feat: add NotFoundEntityException to consult in db

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -2,10 +2,12 @@ package br.com.ifpe.oxefood.modelo.cliente;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.util.EntidadeNaoEncontradaException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -31,7 +33,14 @@ public class ClienteService {
     }
 
     public Cliente obterPorID(Long id) {
-        return repository.findById(id).get();
+
+        Optional<Cliente> consulta = repository.findById(id);
+
+        if(consulta.isPresent()){
+            return consulta.get();
+        } else {
+            throw new EntidadeNaoEncontradaException("Cliente", id);
+        }
     }
 
     @Transactional
